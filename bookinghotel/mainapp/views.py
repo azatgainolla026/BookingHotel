1d587f64fa60db1276bf75631d07ea56f9c74d18
alikhangay <120228097+alikhangay@users.noreply.github.com>
2025-05-12 03:06:32 +0500

    trala lai la trala la

--- bookinghotel/mainapp/views.py
+++ bookinghotel/mainapp/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import requests
 
-def index(request):
-    pass
+def main_page(request):
+    return render(request, 'mainapp/main_page.html')
+def about_page(request):
+    return render(request, 'mainapp/about.html')
+def login_page(request):
+    return render(request, 'mainapp/login.html')
+def register_page(request):
+    return render(request, 'mainapp/register.html')
